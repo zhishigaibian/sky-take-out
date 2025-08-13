13c1c3b47e71992b0c08aef63c6469cfc98df3d1
zjk <50859698@qq.com>
2025-08-13 20:35:24 +0800

    苍穹外卖新增菜品

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,29 @@
+package com.sky.controller.admin;
+
+
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/admin/dish")
+@Api(tags = "菜品相关接口")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+    @PostMapping
+    @ApiOperation("新增菜品")
+    public Result save(){
+        log.info("新增菜品");
+
+
+        return Result.success();
+    }
+}
